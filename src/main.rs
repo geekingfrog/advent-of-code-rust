41e92316e83fc4e55778443a6ff7efcf64900f5b
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2019-12-01 21:00:59 +0000

    here we go again

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate clap;
 use clap::{Arg, App};
 
 mod y2018;
+mod y2019;
 
 fn main() {
     let matches = App::new("Advent of code")
@@ -18,6 +19,7 @@ fn main() {
 
     match year.as_ref() {
         "2018" => run2018(day, pb_number),
+        "2019" => run2019(day, pb_number),
         _ => {
             println!("Unknown year: {}", year);
             std::process::exit(1);
@@ -31,6 +33,14 @@ fn run2018(day: u8, pb_number: u8) {
         12 => y2018::day01::answer2(),
         21 => y2018::day02::answer1(),
         22 => y2018::day02::answer2(),
+        _ => println!("Unknown pair day-pb number: {} - {}", day, pb_number),
+    }
+}
+
+fn run2019(day: u8, pb_number: u8) {
+    match day * 10 + pb_number {
+        11 => y2019::day01::answer1(),
+        12 => y2019::day01::answer2(),
         _ => println!("Unknown pair day-pb number: {} - {}", day, pb_number),
     }
 }
---
+++ src/y2019/day01.rs
@@ -0,0 +1,34 @@
+use std::cmp;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+pub fn answer1() {
+    let res:i32 = read_modules()
+        .map(|x| (x / 3) - 2)
+        .sum();
+    println!("{}", res);
+}
+
+pub fn answer2() {
+    let res:i32 = read_modules()
+        .map(fuel)
+        .sum();
+    println!("{}", res);
+}
+
+fn fuel(module_mass : i32) -> i32 {
+    let mut mass = module_mass;
+    let mut required_fuel = 0;
+    while mass > 0 {
+        mass = cmp::max(mass / 3 - 2, 0);
+        required_fuel += mass;
+    }
+    return required_fuel;
+}
+
+fn read_modules() -> impl Iterator<Item = i32>{
+    let f = File::open("data/2019/day01.txt").unwrap();
+    let fd = BufReader::new(f);
+    return fd.lines().map(|x| i32::from_str_radix(&x.unwrap(), 10).unwrap());
+}
