19db6101153948e2616e7bfb68a6837376a6f6e1
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2019-12-08 08:50:23 +0000

    day5

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,8 @@ fn run2019(day: u8, pb_number: u8) {
         32 => y2019::day03::answer2(),
         41 => y2019::day04::answer1(),
         42 => y2019::day04::answer2(),
+        51 => y2019::day05::answer1(),
+        52 => y2019::day05::answer2(),
         _ => println!("Unknown pair day-pb number: {} - {}", day, pb_number),
     }
 }
